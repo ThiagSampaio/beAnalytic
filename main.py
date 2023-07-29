58d07c7e0ba00c7b05fa526e40b14e6ec585ec7b
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2023-07-29 15:27:02 -0300

    Finaliza a busca de todos jogos steam

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import pandas as pd
 import mysql.connector
 from mysql.connector import Error
+import pymysql
+
+
 
 # ---- Puxando infos da api --------------
 def getInfo1():
@@ -13,43 +16,46 @@ def getInfo1():
     teste = pd.json_normalize(new_data)
     
     return teste
+def getInfo2():
+    'Pega as infos para tabela jogo '
+    url = "http://api.steampowered.com/ISteamApps/GetAppList/v0001/"
+    response = requests.get(url)
+    response_json = response.json()
+    new_data = response_json['applist']['apps']['app']
+    teste = pd.json_normalize(new_data)
+    
+    return teste
 
+# ---- Joga os dados na tabela -------
 
-
-
-# ----- Operaçoes DB 
-
-def quantidade_info():
-    'Funcao para ver se existe dado no DB'
-
+def putDados1():    
     try:
-        con = mysql.connector.connect(host='localhost',database='beanalytic',user='root',password='e2h8Ta6i9qnUPt')
-        consulta_sql = "select * from jogo"
-        cursor = con.cursor()
-        cursor.execute(consulta_sql)
-        linhas = cursor.fetchall()
-        #print("Número total de registros retornados: ", cursor.rowcount)
-        if cursor.rowcount > 0 :
-            if (con.is_connected()):
-                con.close()
-                cursor.close()
-                #print("Conexão ao MySQL encerrada")
-            return True
+        dataframe = getInfo1()
+        db = mysql.connector.connect(host='localhost',database='beanalytic',user='root',password='e2h8Ta6i9qnUPt')
+        if(db):
+            print('Conect')
         else:
-            if (con.is_connected()):
-                con.close()
-                cursor.close()
-                #print("Conexão ao MySQL encerrada")
-            return False
+            print('falha')
+        cur = db.cursor()
+        cur.execute('DELETE from jogo')
+
+        query = db.cursor()
+
+        for(row, rs) in dataframe.iterrows():
+            appid = rs[0]
+            name = rs[1]
+            syntext = """INSERT INTO jogo (appid, name) VALUES (%s,%s)"""
+            value = (appid,name)
+            query.execute(syntext,value)
+            db.commit()
 
     except Error as e:
         print("Erro ao acessar tabela MySQL", e)
     finally:
-        if (con.is_connected()):
-            con.close()
-            cursor.close()
-            print("Conexão ao MySQL encerrada")
-
-teste = quantidade_info()
-print(teste)
+        if (db.is_connected()):
+                db.close()
+                db.close()
+                print("Conexão ao MySQL encerrada")
+    
+putDados1()
 
